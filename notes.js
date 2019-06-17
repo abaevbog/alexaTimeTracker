d55981c163536fe24079bb50b99fe106c08ec82f
Bogdan Abaev <abaevbog@grinnell.edu>
2019-06-17 17:09:44 -0400

    added duration of work

--- notes.js
+++ notes.js
@@ -24,7 +24,29 @@ const addLog = function (projectName, activity) {
         return projectDict.project == projectName;
     })
     if (projIndex != -1) {
-        activity == 'start' ? data[projIndex].logs.push({ start: timestamp('YYYY/MM/DD/HH:mm:ss') }) : data[projIndex].logs[data[projIndex].logs.length-1].end = timestamp('YYYY/MM/DD/HH:mm:ss');
+        const logsLen = data[projIndex].logs.length-1;
+        if (activity == 'start'){
+            if (typeof data[projIndex].logs[logsLen].start != 'undefined' &&
+                typeof data[projIndex].logs[logsLen].end != 'undefined' || 
+                logsLen == 0){
+                data[projIndex].logs.push({ start: timestamp('YYYY/MM/DD/HH:mm:ss') });
+            } else{
+                console.log("Work on this project has already been started");
+            }
+         } else {
+             if (typeof data[projIndex].logs[logsLen].start != 'undefined'){
+                if (typeof data[projIndex].logs[logsLen].end == 'undefined'){
+                    data[projIndex].logs[logsLen].end = timestamp('YYYY/MM/DD/HH:mm:ss');
+                    data[projIndex].logs[logsLen].duration = subtractTimeStamps(data[projIndex].logs[logsLen].end,data[projIndex].logs[logsLen].start); 
+                    console.log("Work finished");
+                } else {
+                    console.log("Work on this project has already been finished");
+                }
+             } else {
+                 console.log("Work on this project was never started");
+             }
+             
+         }
         saveProjects(data);
     } else {
         console.log("Project not found");
@@ -68,8 +90,31 @@ const listProjects = function () {
     const data = loadLogs();
     for (var i = 0; i < data.length; i++) {
        console.log(`${data[i].project}\t`); 
+       subtractTimeStamps(data[i].logs[0].start,data[i].logs[0].end);
     }
 
+}
+
+
+const subtractTimeStamps = function(stampTwo,stampOne){
+    const timeOne = stampOne.toString().substring(11,stampOne.length);
+    const timeTwo = stampTwo.toString().substring(11,stampTwo.length);
+    const lstOne = timeOne.split(':');
+    const lstTwo = timeTwo.split(':');
+    var durationHour = parseInt(lstTwo[0] - lstOne[0]);
+    var durationMin = parseInt(lstTwo[1] - lstOne[1]);
+    var durationSec = parseInt(lstTwo[2] - lstOne[2]);
+    if (durationMin < 0) {
+        durationHour--;
+        durationMin = 60 + durationMin;
+    }
+    if (durationSec < 0){
+        durationMin--;
+        durationSec = 60 + durationSec;
+    }
+    console.log(`${durationHour}:${durationMin}:${durationSec}`);
+    assert(durationHour >= 0);
+    return `${durationHour}:${durationMin}:${durationSec}`;
 }
 
 
