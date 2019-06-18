e5ea435af8e2c4d51495a1e1d2f2789fb564d046
Bogdan Abaev <abaevbog@grinnell.edu>
2019-06-18 12:39:33 -0400

    added simple reporting + some bugs fixed. Works but in a clunky way

--- app.js
+++ app.js
@@ -4,16 +4,11 @@ const yargs = require('yargs')
 const notes = require('./notes.js')
 
 
+
 yargs.command({
-    'command':'start',
+    'command':'start <project>',
     'describe': 'work on project',
-    'builder':{
-        'project': {
-            'describe':'name of the project to work on',
-            'demandOption':true,
-            'type':'string'
-        }
-    },
+
     handler: function(argv){
         notes.addLog(argv.project,'start');
     }
@@ -22,44 +17,24 @@ yargs.command({
 
 yargs.command({
     'command':'end',
-    'describe': 'finish working on project',
-    'builder':{
-        'project': {
-            'describe':'name of the project to stop working on',
-            'demandOption':true,
-            'type':'string'
-        }
-    },
+    'describe': 'finish working on current project',
+
     handler: function(argv){
         notes.addLog(argv.project, 'end');
     }
 });
 
 yargs.command({
-    'command':'create',
+    'command':'create <project>',
     'describe': 'create new project',
-    'builder': {
-        'project': {
-            'describe': 'New project to create',
-             demandOption:true,
-             type: 'string'
-        },
-    },
     handler: function(argv){
         notes.createProject(argv.project);
     }
 });
 
 yargs.command({
-    'command':'delete',
+    'command':'delete <project>',
     'describe': 'delete project',
-    'builder': {
-        'project': {
-            'describe': 'Delete existing project',
-             demandOption:true,
-             type: 'string'
-        },
-    },
     handler: function(argv){
         notes.removeProject(argv.project);
     }
@@ -71,6 +46,22 @@ yargs.command({
     handler: function(argv){
         console.log("Existing projects:\n");
         notes.listProjects();
+    }
+});
+
+yargs.command({
+    'command':'current',
+    'describe': 'current project',
+    handler: function(argv){
+        notes.currentProject();
+    }
+});
+
+yargs.command({
+    'command':'report <mode>',
+    'describe': 'report daily or weekly work progress',
+    handler: function(argv){
+        notes.report(argv.mode);
     }
 });
 
--- notes.js
+++ notes.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const timestamp = require('time-stamp');
 const assert = require('assert');
 
+var currentProject;
 
 const createProject = function (title) {
     //data - list of projects
@@ -9,9 +10,9 @@ const createProject = function (title) {
     const duplicateProject = data.find((project) => project.title === title);
     if (!duplicateProject) {
         const proj = { "project": title, "logs": [] };
-        console.log(proj);
         data.push(proj);
         saveProjects(data);
+        console.log("Done!");
     } else {
         console.log("Project title already taken");
     }
@@ -20,16 +21,25 @@ const createProject = function (title) {
 const addLog = function (projectName, activity) {
     assert(activity == 'start' || activity == 'end');
     const data = loadLogs();
+    if(activity == 'end'){
+        projectName = currentProject;
+    }
     const projIndex = data.findIndex(function (projectDict) {
         return projectDict.project == projectName;
     })
     if (projIndex != -1) {
         const logsLen = data[projIndex].logs.length-1;
         if (activity == 'start'){
-            if (typeof data[projIndex].logs[logsLen].start != 'undefined' &&
-                typeof data[projIndex].logs[logsLen].end != 'undefined' || 
-                logsLen == 0){
+            if (currentProject){
+                console.log(`You can't start a new project because you are working on ${currentProject}`);
+                return;
+            }
+            if (logsLen  < 1 || typeof data[projIndex].logs[logsLen].start != 'undefined' &&
+                typeof data[projIndex].logs[logsLen].end != 'undefined'){
                 data[projIndex].logs.push({ start: timestamp('YYYY/MM/DD/HH:mm:ss') });
+                console.log(projectName.slice());
+                currentProject = projectName.slice();
+                console.log(currentProject);
             } else{
                 console.log("Work on this project has already been started");
             }
@@ -37,15 +47,15 @@ const addLog = function (projectName, activity) {
              if (typeof data[projIndex].logs[logsLen].start != 'undefined'){
                 if (typeof data[projIndex].logs[logsLen].end == 'undefined'){
                     data[projIndex].logs[logsLen].end = timestamp('YYYY/MM/DD/HH:mm:ss');
-                    data[projIndex].logs[logsLen].duration = subtractTimeStamps(data[projIndex].logs[logsLen].end,data[projIndex].logs[logsLen].start); 
+                    data[projIndex].logs[logsLen].duration = subtractTimeStamps(data[projIndex].logs[logsLen].end,data[projIndex].logs[logsLen].start);
+                    currentProject = ''; 
                     console.log("Work finished");
                 } else {
                     console.log("Work on this project has already been finished");
                 }
              } else {
                  console.log("Work on this project was never started");
-             }
-             
+             }        
          }
         saveProjects(data);
     } else {
@@ -56,15 +66,16 @@ const addLog = function (projectName, activity) {
 
 
 const saveProjects = (projects) => {
-    const dataJson = JSON.stringify(projects);
-    fs.writeFileSync('timetracker.json', dataJson);
+    const dataJson = JSON.stringify( {"current":currentProject,"recorded":projects});
+    fs.writeFileSync('/Users/bogdanabaev/RandomProgramming/node/notes/timetracker.json', dataJson);
 }
 
 const loadLogs = function () {
     try {
-        const buffer = fs.readFileSync('timetracker.json');
+        const buffer = fs.readFileSync('/Users/bogdanabaev/RandomProgramming/node/notes/timetracker.json');
         const dataJson = buffer.toString();
-        return JSON.parse(dataJson);
+        currentProject = JSON.parse(dataJson).current;
+        return JSON.parse(dataJson).recorded;
     } catch (e) {
         return []
     }
@@ -73,14 +84,12 @@ const loadLogs = function () {
 const removeProject = function (projectName) {
     const data = loadLogs();
     const projIndex = data.findIndex(function (projectDict) {
-        console.log(projectDict.project);
-        console.log(projectName);
         return projectDict.project == projectName;
     })
     if (projIndex != -1) {
         data.splice(projIndex, 1);
         saveProjects(data);
-        console.log("success");
+        console.log("Success");
     } else {
         console.log("Note does not exist");
     }
@@ -90,9 +99,25 @@ const listProjects = function () {
     const data = loadLogs();
     for (var i = 0; i < data.length; i++) {
        console.log(`${data[i].project}\t`); 
-       subtractTimeStamps(data[i].logs[0].start,data[i].logs[0].end);
     }
 
+}
+
+
+const getCurrentProjData = function(){
+    const data = loadLogs();
+    if (currentProject){
+        const projIndex = data.findIndex(function (projectDict) {
+            return projectDict.project == currentProject;
+        });
+        assert(projIndex!=-1);
+        const logsLen = data[projIndex].logs.length-1;
+        const duration = subtractTimeStamps(timestamp('YYYY/MM/DD/HH:mm:ss'),data[projIndex].logs[logsLen].start);
+        console.log(`Current project: ${currentProject}`);
+        console.log(`Work time elapsed: ${duration}`);
+    }else {
+        console.log("No current project");
+    }
 }
 
 
@@ -112,9 +137,26 @@ const subtractTimeStamps = function(stampTwo,stampOne){
         durationMin--;
         durationSec = 60 + durationSec;
     }
-    console.log(`${durationHour}:${durationMin}:${durationSec}`);
+    //console.log(`${durationHour}:${durationMin}:${durationSec}`);
     assert(durationHour >= 0);
     return `${durationHour}:${durationMin}:${durationSec}`;
+}
+
+//mode = week or today
+const report = function(mode){
+    const data = loadLogs();
+    const today = timestamp('YYYY/MM/DD');
+    const result = [];
+    if (mode == 'today'){
+        data.forEach(element => {
+            var i = element.logs.length-1;
+            while ( i > -1 && element.logs[i].end && element.logs[i].end.substring(0,10) == today){
+                result.push(`Project ${element.project}: ${element.logs[i].duration}`);
+                i--;
+            }  
+        });
+    }
+    console.log(result);
 }
 
 
@@ -124,5 +166,7 @@ module.exports = {
     createProject: createProject,
     addLog: addLog,
     removeProject: removeProject,
-    listProjects: listProjects
+    listProjects: listProjects,
+    currentProject:getCurrentProjData,
+    report:report
 }
