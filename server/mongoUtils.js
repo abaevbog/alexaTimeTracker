d06eba36fdae5ca61dd93f26fc27420edfba21e7
Bogdan Abaev <abaevbog@grinnell.edu>
2019-07-11 18:45:17 -0700

    minor fix

--- server/mongoUtils.js
+++ server/mongoUtils.js
@@ -2,7 +2,6 @@
 const { MongoClient} = require('mongodb');
 
 const connectionUrl = process.env.DB_URL? process.env.DB_URL: 'mongodb://127.0.0.1:27017/';
-//const connectionUrl = 'mongodb://127.0.0.1:27017';
 
 const database = 'task_manager'
 var db;
